f41f2999fb7a72cbc32f361bd6ca53fdbd7bb91c
Lorenzo Dalmazzo <pollydalmazzo@gmail.com>
2024-04-25 00:23:33 +0200

    use route id instead of url

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,7 +2,8 @@ import { SECRET_PSW } from '$env/static/private';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	if (event.request.url.includes('api/loggedin') || event.request.url.includes('app'))
+	const route = event.route.id;
+	if (route && (route.includes('api/loggedin') || route.includes('app')))
 		if (event.cookies.get('psw') !== SECRET_PSW)
 			return new Response('Not authorized', { status: 401 });
 	return resolve(event);
