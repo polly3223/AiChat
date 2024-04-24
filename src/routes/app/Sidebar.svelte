3651c6f3419e49103dfd58c2e42057c755b1c3ff
Lorenzo Dalmazzo <pollydalmazzo@gmail.com>
2024-04-24 22:48:16 +0200

    Update sidebar with history

--- src/routes/app/Sidebar.svelte
+++ src/routes/app/Sidebar.svelte
@@ -1,3 +1,13 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+
+	function classWhenActive(path: string, currentPath: string) {
+		return path === currentPath
+			? 'bg-gray-800 text-white gap-x-3 rounded-md p-2 text-sm group flex leading-6 font-semibold'
+			: 'text-gray-400 hover:text-white hover:bg-gray-800 gap-x-3 rounded-md p-2 group flex text-sm leading-6 font-semibold';
+	}
+</script>
+
 <div class="flex h-16 shrink-0 items-center">
 	<img
 		class="h-8 w-auto"
@@ -10,185 +20,47 @@
 		<li>
 			<ul role="list" class="-mx-2 space-y-1">
 				<li>
-					<!-- Current: "bg-gray-800 text-white", Default: "text-gray-400 hover:text-white hover:bg-gray-800" -->
-					<a
-						href="/"
-						class="bg-gray-800 text-white group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-					>
+					<a href="/app" class={classWhenActive('/app', $page.url.pathname)}>
 						<svg
-							class="h-6 w-6 shrink-0"
+							xmlns="http://www.w3.org/2000/svg"
 							fill="none"
 							viewBox="0 0 24 24"
 							stroke-width="1.5"
 							stroke="currentColor"
-							aria-hidden="true"
+							class="h-6 w-6 shrink-0"
 						>
 							<path
 								stroke-linecap="round"
 								stroke-linejoin="round"
-								d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25"
+								d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"
 							/>
 						</svg>
-						Dashboard
+
+						Chat
 					</a>
 				</li>
 				<li>
-					<a
-						href="/"
-						class="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-					>
+					<a href="/app/history" class={classWhenActive('/app/history', $page.url.pathname)}>
 						<svg
-							class="h-6 w-6 shrink-0"
+							xmlns="http://www.w3.org/2000/svg"
 							fill="none"
 							viewBox="0 0 24 24"
 							stroke-width="1.5"
 							stroke="currentColor"
-							aria-hidden="true"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
-							/>
-						</svg>
-						Team
-					</a>
-				</li>
-				<li>
-					<a
-						href="/"
-						class="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-					>
-						<svg
 							class="h-6 w-6 shrink-0"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							aria-hidden="true"
 						>
 							<path
 								stroke-linecap="round"
 								stroke-linejoin="round"
-								d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
+								d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z"
 							/>
 						</svg>
-						Projects
+
+						History
 					</a>
 				</li>
-				<li>
-					<a
-						href="/"
-						class="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-					>
-						<svg
-							class="h-6 w-6 shrink-0"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							aria-hidden="true"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 012.25-2.25h13.5A2.25 2.25 0 0121 7.5v11.25m-18 0A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75m-18 0v-7.5A2.25 2.25 0 015.25 9h13.5A2.25 2.25 0 0121 11.25v7.5"
-							/>
-						</svg>
-						Calendar
-					</a>
-				</li>
-				<li>
-					<a
-						href="/"
-						class="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-					>
-						<svg
-							class="h-6 w-6 shrink-0"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							aria-hidden="true"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 01-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 011.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 00-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 01-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 00-3.375-3.375h-1.5a1.125 1.125 0 01-1.125-1.125v-1.5a3.375 3.375 0 00-3.375-3.375H9.75"
-							/>
-						</svg>
-						Documents
-					</a>
-				</li>
-				<li>
-					<a
-						href="/"
-						class="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-					>
-						<svg
-							class="h-6 w-6 shrink-0"
-							fill="none"
-							viewBox="0 0 24 24"
-							stroke-width="1.5"
-							stroke="currentColor"
-							aria-hidden="true"
-						>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M10.5 6a7.5 7.5 0 107.5 7.5h-7.5V6z"
-							/>
-							<path
-								stroke-linecap="round"
-								stroke-linejoin="round"
-								d="M13.5 10.5H21A7.5 7.5 0 0013.5 3v7.5z"
-							/>
-						</svg>
-						Reports
-					</a>
-				</li>
-			</ul>
-		</li>
-		<li>
-			<div class="text-xs font-semibold leading-6 text-gray-400">Your chats</div>
-			<ul role="list" class="-mx-2 mt-2 space-y-1">
-				<li>
-					<!-- Current: "bg-gray-800 text-white", Default: "text-gray-400 hover:text-white hover:bg-gray-800" -->
-					<a
-						href="/"
-						class="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-					>
-						<span
-							class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white"
-							>H</span
-						>
-						<span class="truncate">Heroicons</span>
-					</a>
-				</li>
-				<li>
-					<a
-						href="/"
-						class="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-					>
-						<span
-							class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white"
-							>T</span
-						>
-						<span class="truncate">Tailwind Labs</span>
-					</a>
-				</li>
-				<li>
-					<a
-						href="/"
-						class="text-gray-400 hover:text-white hover:bg-gray-800 group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-					>
-						<span
-							class="flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-700 bg-gray-800 text-[0.625rem] font-medium text-gray-400 group-hover:text-white"
-							>W</span
-						>
-						<span class="truncate">Workcation</span>
-					</a>
-				</li>
+
+				{$page.url.pathname}
 			</ul>
 		</li>
 	</ul>
---
+++ src/routes/app/history/+page.svelte
@@ -0,0 +1,5 @@
+<div class="flex flex-col h-[calc(100dvh-56px)] lg:h-dvh">
+	<div class="overflow-auto p-2">
+		<p>History</p>
+	</div>
+</div>
