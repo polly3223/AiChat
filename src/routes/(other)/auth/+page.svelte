7f59ee009f9a6f30e1b3b1e005811868184cd26a
Lorenzo Dalmazzo <pollydalmazzo@gmail.com>
2024-04-26 18:17:03 +0200

    Create loading page

---
+++ src/routes/(other)/auth/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	let psw = '';
+
+	function login() {
+		fetch('/api/auth', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ psw })
+		})
+			.then((res) => res.json())
+			.then((data) => {
+				if (data.success) {
+					goto('/app');
+				}
+			});
+	}
+</script>
+
+<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-8">
+	<div class="mx-auto max-w-xs text-center">
+		<div class="relative pb-4">
+			<label
+				for="Password"
+				class="absolute -top-2 left-2 inline-block bg-white px-1 text-xs font-medium text-gray-900"
+				>Password</label
+			>
+			<input
+				type="text"
+				name="Password"
+				id="Password"
+				bind:value={psw}
+				class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring- -600 sm:text-sm sm:leading-6"
+				placeholder="psw"
+			/>
+		</div>
+		<button
+			type="button"
+			on:click={login}
+			class="inline-block rounded-md bg-blue-100 px-2.5 py-1.5 text-sm font-semibold text-blue-600 shadow-sm hover:bg-blue-200"
+			>Log In</button
+		>
+	</div>
+</div>
--- src/routes/api/auth/+server.ts
+++ src/routes/api/auth/+server.ts
@@ -5,16 +5,19 @@ import { SECRET_PSW } from '$env/static/private';
 export const POST: RequestHandler = async ({ request, cookies }) => {
 	if (cookies.get('psw') === SECRET_PSW) return json({ success: true });
 
-	const x = await request.json();
-	const req = reqAuth.safeParse(x);
-	if (!req.success) return json({ success: false });
-	if (req.data.psw !== SECRET_PSW) return json({ success: false });
+	try {
+		const x = await request.json();
+		const req = reqAuth.safeParse(x);
+		if (!req.success) return json({ success: false });
+		if (req.data.psw !== SECRET_PSW) return json({ success: false });
 
-	// Set the cookie to expire in 30 days
-	const expiryDate = new Date();
-	expiryDate.setDate(expiryDate.getDate() + 30);
+		// Set the cookie to expire in 30 days
+		const expiryDate = new Date();
+		expiryDate.setDate(expiryDate.getDate() + 30);
+		cookies.set('psw', req.data.psw, { path: '/', expires: expiryDate });
 
-	cookies.set('psw', req.data.psw, { path: '/', expires: expiryDate });
-
-	return json({ success: true });
+		return json({ success: true });
+	} catch (err) {
+		return json({ success: false });
+	}
 };
