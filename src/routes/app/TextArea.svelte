5ddabdb371930b31331a7bda3e5a6ac901ead6ec
Lorenzo Dalmazzo <pollydalmazzo@gmail.com>
2024-04-27 22:56:02 +0200

    set better clear chat btn

--- src/routes/app/TextArea.svelte
+++ src/routes/app/TextArea.svelte
@@ -39,10 +39,10 @@
 		</form>
 	</div>
 </div>
-<div class="flex justify-center pt-4 gap-4">
+<div class="flex justify-center pt-2 gap-2">
 	<button
 		on:click={clearChat}
-		class="inline-flex items-center align-items-center justify-center rounded-md bg-blue-100 px-2.5 py-1.5 text-sm font-semibold text-blue-600 shadow-sm hover:bg-blue-200"
+		class="min-w-36 inline-flex items-center align-items-center justify-center rounded-md bg-red-100 px-2.5 py-1.5 text-sm font-semibold text-red-600 shadow-sm hover:bg-red-200"
 	>
 		Clear Chat
 		<svg
@@ -52,13 +52,15 @@
 			class="w-4 h-4 ml-1"
 		>
 			<path
-				d="M2.87 2.298a.75.75 0 0 0-.812 1.021L3.39 6.624a1 1 0 0 0 .928.626H8.25a.75.75 0 0 1 0 1.5H4.318a1 1 0 0 0-.927.626l-1.333 3.305a.75.75 0 0 0 .811 1.022 24.89 24.89 0 0 0 11.668-5.115.75.75 0 0 0 0-1.175A24.89 24.89 0 0 0 2.869 2.298Z"
+				fill-rule="evenodd"
+				d="M5 3.25V4H2.75a.75.75 0 0 0 0 1.5h.3l.815 8.15A1.5 1.5 0 0 0 5.357 15h5.285a1.5 1.5 0 0 0 1.493-1.35l.815-8.15h.3a.75.75 0 0 0 0-1.5H11v-.75A2.25 2.25 0 0 0 8.75 1h-1.5A2.25 2.25 0 0 0 5 3.25Zm2.25-.75a.75.75 0 0 0-.75.75V4h3v-.75a.75.75 0 0 0-.75-.75h-1.5ZM6.05 6a.75.75 0 0 1 .787.713l.275 5.5a.75.75 0 0 1-1.498.075l-.275-5.5A.75.75 0 0 1 6.05 6Zm3.9 0a.75.75 0 0 1 .712.787l-.275 5.5a.75.75 0 0 1-1.498-.075l.275-5.5a.75.75 0 0 1 .786-.711Z"
+				clip-rule="evenodd"
 			/>
 		</svg>
 	</button>
 	<button
 		on:click={sendMessage}
-		class="inline-flex items-center align-items-center justify-center rounded-md bg-blue-100 px-2.5 py-1.5 text-sm font-semibold text-blue-600 shadow-sm hover:bg-blue-200"
+		class="min-w-36 inline-flex items-center align-items-center justify-center rounded-md bg-blue-100 px-2.5 py-1.5 text-sm font-semibold text-blue-600 shadow-sm hover:bg-blue-200"
 	>
 		Send message
 		<svg
