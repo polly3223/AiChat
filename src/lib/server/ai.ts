1053f47390f49e4b3a8ac06d9e07324298914479
Lorenzo Dalmazzo <pollydalmazzo@gmail.com>
2024-05-07 17:59:13 +0200

    first api tools

--- src/lib/server/ai.ts
+++ src/lib/server/ai.ts
@@ -1,5 +1,25 @@
 import { type Message, type Chat } from '../client/types';
-import { aiClient } from './aiAdapter';
+import { aiClient, type Tool } from './aiAdapter';
+
+const tools: Tool[] = [
+	{
+		type: 'function',
+		function: {
+			name: 'get_game_score',
+			description: 'Get the score for a given NBA game',
+			parameters: {
+				type: 'object',
+				properties: {
+					team_name: {
+						type: 'string',
+						description: "The name of the NBA team (e.g. 'Golden State Warriors')"
+					}
+				},
+				required: ['team_name']
+			}
+		}
+	}
+];
 
 const chatSystemMsg = `You are an helpful assistant.
 IMPORTANT: When you place code in the message, use the code block syntax specifing the language like this:
@@ -12,7 +32,7 @@ so that the code is syntax highlighted.`;
 
 export async function aiChatMsg(chat: Chat): Promise<Message | null> {
 	const msgList: Message[] = [{ role: 'system', content: chatSystemMsg }, ...chat.messages];
-	const content = await aiClient.advancedCompletion('ChatMsg', msgList);
+	const content = await aiClient.advancedCompletion('ChatMsg', msgList, tools);
 	if (!content) return null;
 	return { role: 'assistant', content };
 }
--- src/lib/server/aiAdapter.ts
+++ src/lib/server/aiAdapter.ts
@@ -4,9 +4,31 @@ import { insertLog } from './mongo';
 import Groq from 'groq-sdk';
 import OpenAI from 'openai';
 
+export type Tool = {
+	type: 'function';
+	function: {
+		name: string;
+		description: string;
+		parameters: {
+			type: 'object';
+			properties: {
+				[key: string]: {
+					type: string;
+					description: string;
+				};
+			};
+			required: string[];
+		};
+	};
+};
+
 interface AiClient {
 	simpleCompletion: (operation: string, messages: Message[]) => Promise<string | null>;
-	advancedCompletion: (operation: string, messages: Message[]) => Promise<string | null>;
+	advancedCompletion: (
+		operation: string,
+		messages: Message[],
+		tools?: Tool[]
+	) => Promise<string | null>;
 }
 
 class GroqClient implements AiClient {
@@ -26,12 +48,24 @@ class GroqClient implements AiClient {
 		}
 	}
 
-	async advancedCompletion(operation: string, messages: Message[]): Promise<string | null> {
+	async advancedCompletion(
+		operation: string,
+		messages: Message[],
+		tools?: Tool[]
+	): Promise<string | null> {
 		const model = 'llama3-70b-8192';
 		const apiProvider = 'groq';
 		try {
-			const response = await this.client.chat.completions.create({ model, messages });
-			const content = response.choices[0].message.content;
+			const response = await this.client.chat.completions.create({
+				model,
+				messages,
+				tools
+			});
+			let content = response.choices[0].message.content;
+			const tool_calls = response.choices[0].message.tool_calls;
+			if (tool_calls) {
+				content = JSON.stringify(tool_calls, null, 2);
+			}
 			insertLog({ model, apiProvider, operation, messages, usage: response.usage, res: content });
 			return content;
 		} catch (error: any) {
@@ -56,25 +90,39 @@ class ToghetherAiClient implements AiClient {
 			insertLog({ model, apiProvider, operation, messages, usage: response.usage, res: content });
 			return content;
 		} catch (error: any) {
-			insertLog({ model, apiProvider, operation, messages, res: error.error });
+			insertLog({ model, apiProvider, operation, messages, res: error });
 			return null;
 		}
 	}
 
-	async advancedCompletion(operation: string, messages: Message[]): Promise<string | null> {
-		const model = 'meta-llama/Llama-3-70b-chat-hf';
+	async advancedCompletion(
+		operation: string,
+		messages: Message[],
+		tools?: Tool[]
+	): Promise<string | null> {
+		// const model = 'meta-llama/Llama-3-70b-chat-hf'; // This is better but can't handle tools for now
+		const model = 'togethercomputer/CodeLlama-34b-Instruct';
 		const apiProvider = 'TogetherAI';
 		try {
-			const response = await this.client.chat.completions.create({ model, messages });
-			const content = response.choices[0].message.content;
+			const response = await this.client.chat.completions.create({
+				model,
+				messages,
+				tools,
+				tool_choice: 'auto'
+			});
+			let content = response.choices[0].message.content;
+			const tool_calls = response.choices[0].message.tool_calls;
+			if (tool_calls) {
+				content += JSON.stringify(tool_calls, null, 2);
+			}
 			insertLog({ model, apiProvider, operation, messages, usage: response.usage, res: content });
 			return content;
 		} catch (error: any) {
-			insertLog({ model, apiProvider, operation, messages, res: error.error });
+			insertLog({ model, apiProvider, operation, messages, res: error });
 			return null;
 		}
 	}
 }
 
-// export const aiClient: AiClient = new GroqClient();
-export const aiClient: AiClient = new ToghetherAiClient();
+export const aiClient: AiClient = new GroqClient();
+// export const aiClient: AiClient = new ToghetherAiClient();
