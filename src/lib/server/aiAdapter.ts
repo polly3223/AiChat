798c3fcf4fdb052fd7f073930e6e8477fb71a62c
Lorenzo Dalmazzo <pollydalmazzo@gmail.com>
2024-05-07 16:42:50 +0200

    Use both groq and toghether ai

--- src/lib/server/aiAdapter.ts
+++ src/lib/server/aiAdapter.ts
@@ -1,7 +1,8 @@
-import { SECRET_GROQ_API_KEY } from '$env/static/private';
+import { SECRET_GROQ_API_KEY, SECRET_TOGHETERAI_API_KEY } from '$env/static/private';
 import { type Message } from '../client/types';
 import { insertLog } from './mongo';
 import Groq from 'groq-sdk';
+import OpenAI from 'openai';
 
 interface AiClient {
 	simpleCompletion: (operation: string, messages: Message[]) => Promise<string | null>;
@@ -40,4 +41,40 @@ class GroqClient implements AiClient {
 	}
 }
 
-export const aiClient: AiClient = new GroqClient();
+class ToghetherAiClient implements AiClient {
+	private client = new OpenAI({
+		apiKey: SECRET_TOGHETERAI_API_KEY,
+		baseURL: 'https://api.together.xyz/v1'
+	});
+
+	async simpleCompletion(operation: string, messages: Message[]): Promise<string | null> {
+		const model = 'meta-llama/Llama-3-8b-chat-hf';
+		const apiProvider = 'TogetherAI';
+		try {
+			const response = await this.client.chat.completions.create({ model, messages });
+			const content = response.choices[0].message.content;
+			insertLog({ model, apiProvider, operation, messages, usage: response.usage, res: content });
+			return content;
+		} catch (error: any) {
+			insertLog({ model, apiProvider, operation, messages, res: error.error });
+			return null;
+		}
+	}
+
+	async advancedCompletion(operation: string, messages: Message[]): Promise<string | null> {
+		const model = 'meta-llama/Llama-3-70b-chat-hf';
+		const apiProvider = 'TogetherAI';
+		try {
+			const response = await this.client.chat.completions.create({ model, messages });
+			const content = response.choices[0].message.content;
+			insertLog({ model, apiProvider, operation, messages, usage: response.usage, res: content });
+			return content;
+		} catch (error: any) {
+			insertLog({ model, apiProvider, operation, messages, res: error.error });
+			return null;
+		}
+	}
+}
+
+// export const aiClient: AiClient = new GroqClient();
+export const aiClient: AiClient = new ToghetherAiClient();
